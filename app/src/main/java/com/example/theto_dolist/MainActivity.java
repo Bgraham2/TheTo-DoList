06df08054ec54ac7aa9117c371abdf7a2b6ca576
BrianGraham <bgraham2@gmail.com>
2023-04-06 07:45:53 -0500

    Removed hardcoded text and add dialog fragment

--- app/src/main/java/com/example/theto_dolist/MainActivity.java
+++ app/src/main/java/com/example/theto_dolist/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.theto_dolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textViewLabel);
+        textView.setText(new SimpleDateFormat("EEE, d MMM yyyy", Locale.getDefault()).format(new Date()));
+
+        Button button = findViewById(R.id.buttonAddTask);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
